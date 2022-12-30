829b62b278febcf3b7f66140d63584aec0cea783
Franco <72584397+erekkles@users.noreply.github.com>
2022-12-30 00:02:52 -0300

    Simplified Vector2D show method

--- canvas-api/vector2d.js
+++ canvas-api/vector2d.js
@@ -40,7 +40,6 @@ export class Vector2D {
         return new Vector2D(vector.x / factor, vector.y / factor);
     }
    
-    // https://en.wikipedia.org/wiki/Pythagorean_theorem
     mag() { 
         return Math.sqrt(this.x**2 + this.y**2); // since it's exponential operation, the value will always be +
     }
@@ -69,31 +68,13 @@ export class Vector2D {
             starting_y_point = assigned_shape.position.y;
         }
 
-        let final_x_point;
-        let final_y_point;
-
-        if(this.x === 0) {
-            final_x_point = starting_x_point;
-        } else {
-            final_x_point = this.x < 0 ? starting_x_point + this.mag() : starting_x_point - this.mag();
-        }
-
-        if(this.y === 0) {
-            final_y_point = starting_y_point;
-        } else {
-            final_y_point = this.y < 0 ? starting_y_point + this.mag() : starting_y_point - this.mag();
-        }
+        let final_x_point = starting_x_point + this.x;
+        let final_y_point = starting_y_point + this.y;
         
-        canvas_context.strokeStyle = 'red';
+        canvas_context.strokeStyle = 'black';
         canvas_context.beginPath();
         canvas_context.moveTo(starting_x_point, starting_y_point);
         canvas_context.lineTo(final_x_point, final_y_point);
         canvas_context.stroke();
     }
 }
-
-/*
-    References:
-        - https://en.wikipedia.org/wiki/Euclidean_vector#:~:text=In%20mathematics%2C%20physics%2C%20and%20engineering,vectors%20according%20to%20vector%20algebra.
-        - https://processing.org/reference/PVector.html
-*/
